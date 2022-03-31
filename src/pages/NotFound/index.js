fdb238289972a46ac54e6cbedaf26f78f9a5191e
DD <50895419+DD513@users.noreply.github.com>
2022-03-31 14:14:16 +0800

    feat: :sparkles: add goback & goHomePage #16

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -2,21 +2,37 @@ import React from "react";
 import "./index.less";
 import images from "../../config/images";
 import { Button, Row, Col } from "antd";
+import { useNavigate } from "react-router-dom";
 
 function NotFoundPage() {
+  let navigate = useNavigate();
   return (
     <div className="NotFoundContainer">
       <Row justify="center">
         <Col xs={24} className="imgBlock">
-          <img src={images.NotFound} className="NotFoundImage" />
+          <img
+            src={images.NotFound}
+            className="NotFoundImage"
+            alt="NotFoundImage"
+          />
         </Col>
-        <Col xs={24} sm={6} className="buttonBlock">
-          <Button ghost className="globalButton">
+        <Col xs={18} sm={6} className="buttonBlock">
+          <Button
+            ghost
+            className="globalButton"
+            size="large"
+            onClick={() => navigate(-1)}
+          >
             回上一頁
           </Button>
         </Col>
-        <Col xs={24} sm={6} className="buttonBlock">
-          <Button type="dashed" ghost className="globalButton">
+        <Col xs={18} sm={6} className="buttonBlock">
+          <Button
+            ghost
+            className="globalButton"
+            size="large"
+            onClick={() => navigate("/")}
+          >
             回首頁
           </Button>
         </Col>
@@ -26,16 +42,3 @@ function NotFoundPage() {
 }
 
 export default NotFoundPage;
-
-// <Row justify="center">
-//             <Col xs={24} sm={12}>
-//               <Button ghost className="globalButton">
-//                 回上一頁
-//               </Button>
-//             </Col>
-//             <Col xs={24} sm={12}>
-//               <Button type="dashed" ghost className="globalButton">
-//                 回首頁
-//               </Button>
-//             </Col>
-//           </Row>
